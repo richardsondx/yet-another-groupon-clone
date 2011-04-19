5a9bc5e87268861348c3c9cc6b43ffd8ba70fde5
Michal Szyndel <szyndel@gmail.com>
2011-04-19 18:09:22 +0200

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupon::Application.config.secret_token = 'f353d26012d6428250118114dcc955a78e0ecf59f73f326511ba28cb1aca43d828cc5ba7c95273a104b426f14feb5c36ac0055b8862a7aae758d0d08e4854cd3'
